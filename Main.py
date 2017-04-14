3e83fac6bbee9bfcc3828bfeb5c48bed5641de82
liu.z.y <yad50968@gmail.com>
2017-04-14 22:46:54 +0800

    Change to classfication Without use validation data

--- Main.py
+++ Main.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import math
 from time import time
 import numpy as np
-import matplotlib.pyplot as plt
 from pyspark import SparkConf, SparkContext
 from pyspark.mllib.tree import DecisionTree
 from pyspark.mllib.regression import LabeledPoint
@@ -29,7 +26,7 @@ def convert_float(x):
 
 
 def extract_features(record):
-    features_index = [3, 4, 5, 14, 15, 16, 25]
+    features_index = [3, 4, 5, 14, 15, 16, 25, -2, -3]
     features = [convert_float(record[x]) for x in features_index]
     return np.asarray(features)
 
@@ -42,11 +39,10 @@ def prepare_data(sc):
     lines = rawData.map(lambda x: x.split(","))
     print (lines.first())
     print("共計：" + str(lines.count()) + "筆")
-    labelpointRDD = lines.map(lambda r: LabeledPoint(1.0, extract_features(r)))
+    labelpointRDD = lines.map(lambda r: LabeledPoint(extract_label(r), extract_features(r)))
 
-    print labelpointRDD.first()
     (trainData, validationData,
-     testData) = labelpointRDD.randomSplit([8, 1, 1])
+     testData) = labelpointRDD.randomSplit([3, 0, 7])
     print("將資料分trainData:" + str(trainData.count()) +
           "validationData:" + str(validationData.count()) +
           "testData:" + str(testData.count()))
@@ -62,7 +58,20 @@ def CreateSparkContext():
     SetLogger(sc)
     return (sc)
 
+def trainEvaluateModel(trainData,impurity,maxDepth, maxBins):
 
+    model = DecisionTree.trainClassifier(trainData, numClasses=2, categoricalFeaturesInfo={}, impurity=impurity, maxDepth=maxDepth, maxBins=maxBins)
+    return model
+def evaluateModel(model, validationData):
+    score = model.predict(validationData.map(lambda p: p.features))
+    labelsAndPredictions = validationData.map(lambda p: p.label).zip(score)
+    testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(validationData.count())
+    print('Test Error = ' + str(testErr))
+    print('Learned classification tree model:')
+    print(model.toDebugString())
 if __name__ == "__main__":
     sc = CreateSparkContext()
     (trainData, validationData, testData) = prepare_data(sc)
+    trainData.persist(); validationData.persist(); testData.persist()
+    model = trainEvaluateModel(trainData, "gini", 10, 100)
+    evaluateModel(model, testData)
