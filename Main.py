b57188f81d4697318d28627b56c2d9d8feaab9e5
liu.z.y <yad50968@gmail.com>
2017-04-15 18:52:18 +0800

    Add predictData function Same for the prepareData, but set label = 0.0

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import math
 from time import time
 import numpy as np
 from pyspark import SparkConf, SparkContext
@@ -26,14 +26,15 @@ def convert_float(x):
 
 
 def extract_features(record):
-    features_index = [3, 4, 5, 14, 15, 16, 25, -2, -3]
+    features_index = [3, 4, 5, 14, 15, 16, 25]
     features = [convert_float(record[x]) for x in features_index]
     return np.asarray(features)
 
 
+
 def prepare_data(sc):
     print("開始匯入資料...")
-    rawDataWithHeader = sc.textFile("./train.csv")
+    rawDataWithHeader = sc.textFile("./train_100mb.csv")
     header = rawDataWithHeader.first()
     rawData = rawDataWithHeader.filter(lambda x: x != header)
     lines = rawData.map(lambda x: x.split(","))
@@ -74,6 +75,30 @@ def evaluateModel(model, validationData):
     print(model.toDebugString())
 
 
+
+
+def predictData(sc, model):
+    #----------------------1.匯入並轉換資料-------------
+    print("開始匯入資料...")
+    rawDataWithHeader = sc.textFile("./train_100mb.csv")
+    header = rawDataWithHeader.first()
+    rawData = rawDataWithHeader.filter(lambda x:x !=header)
+    lines = rawData.map(lambda x: x.split(","))
+    print("共計：" + str(lines.count()) + "筆")
+    #----------------------2.建立訓練評估所需資料 LabeledPoint RDD-------------
+    labelpointRDD = lines.map(lambda r: LabeledPoint("0.0", extract_features(r)))
+    
+    #----------------------4.進行預測並顯示結果--------------
+
+    # 把預測結果寫出來
+    f = open('workfile', 'w')
+    for lp in labelpointRDD.take(1000):
+        predict = int(model.predict(lp.features))
+        dataDesc = "  " + str(predict) + " "
+        f.write(dataDesc)
+    f.close()
+
+
 if __name__ == "__main__":
     sc = CreateSparkContext()
     (trainData, validationData, testData) = prepare_data(sc)
@@ -82,3 +107,4 @@ if __name__ == "__main__":
     testData.persist()
     model = trainEvaluateModel(trainData, "gini", 10, 100)
     evaluateModel(model, testData)
+    predictData(sc, model)
