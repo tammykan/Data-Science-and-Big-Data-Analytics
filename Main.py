b26576279cc82971c736ec98e0599dff798cb26e
tammykan <tammy.xtech@gmail.com>
2017-06-20 20:56:45 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -24,17 +24,17 @@ def SetLogger(sc):
 
 
 def extract_label(record):
-    label = record[-3]
+    label = record[-1]
     return float(label)
 
 
 def convert_float(x):
     return 0 if x == "NULL" else float(x)
 
-
+#[3, 4, 5, 8, 9, 15, 16, 25]
 def extract_features(record):
     #index = np.arange(51)
-    features_index = [3, 4, 5, 8, 9, 15, 16, 25]
+    features_index = [4, 8, 9, 10, 11, 12, 16, 26]
     #features_index = index[2:51]
     features = [convert_float(record[x]) for x in features_index]
     return np.asarray(features)
@@ -43,7 +43,7 @@ def extract_features(record):
 
 def prepare_data(sc):
     print("開始匯入資料...")
-    rawDataWithHeader = sc.textFile("./train.csv")
+    rawDataWithHeader = sc.textFile("./data.csv")
     header = rawDataWithHeader.first()
     rawData = rawDataWithHeader.filter(lambda x: x != header)
     lines = rawData.map(lambda x: x.split(","))
@@ -60,7 +60,7 @@ def prepare_data(sc):
 
 
 def CreateSparkContext():
-    sparkConf = SparkConf()   \
+    sparkConf = SparkConf() \
         .setAppName("RunRandomForestClassficaiton") \
         .set("spark.ui.showConsoleProgress", "false")
     sc = SparkContext(conf=sparkConf)
@@ -70,15 +70,16 @@ def CreateSparkContext():
 
 
 def trainEvaluateModel(trainData):
-    model = RandomForest.trainClassifier(trainData, 2, {}, 3)
+    model = RandomForest.trainClassifier(trainData, 2, categoricalFeaturesInfo={},
+                                     numTrees=15, featureSubsetStrategy="auto",
+                                     impurity='gini', maxDepth=12, maxBins=32)
     return model
 
 
 def evaluateModel(model, valiadationData):
     score = model.predict(validationData.map(lambda p: p.features))
     labelsAndPredictions = validationData.map(lambda p: (p.label)).zip(score)
-    #labelsAndPredictions = validationData.map(lambda p: (p.label, float(model.predict(p.features))))
-
+   
     testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(validationData.count())
 
     print('Test Error = ' + str(testErr))
@@ -99,10 +100,10 @@ def rocCurve(model, testData):
     metrics = BinaryClassificationMetrics(labels_and_predictions)
 
     # Area under precision-recall curve
-    print("Area under Precision/Recall (PR) curve: %.f" % (metrics.areaUnderPR * 100))
+    print("Area under Precision/Recall (PR) curve: %.f %%" % (metrics.areaUnderPR * 100))
 
     # Area under ROC curve
-    print("Area under Receiver Operating Characteristic (ROC) curve: %.3f" % (metrics.areaUnderROC * 100))
+    print("Area under Receiver Operating Characteristic (ROC) curve: %.3f %%" % (metrics.areaUnderROC * 100))
 
     end_time = time()
     elapsed_time = end_time - start_time
@@ -111,7 +112,7 @@ def rocCurve(model, testData):
 def predictData(sc, model):
     #----------------------1.匯入並轉換資料-------------
     print("開始匯入資料...")
-    rawDataWithHeader = sc.textFile("./train.csv")
+    rawDataWithHeader = sc.textFile("./data.csv")
     header = rawDataWithHeader.first()
     rawData = rawDataWithHeader.filter(lambda x:x !=header)
     lines = rawData.map(lambda x: x.split(","))
@@ -132,24 +133,24 @@ def predictData(sc, model):
 
 
 if __name__ == "__main__":
+
     sc = CreateSparkContext()
-    k = 3
     (trainData, validationData, testData) = prepare_data(sc)
     trainData.persist()
     validationData.persist()
     testData.persist()
     modelBest = trainEvaluateModel(trainData)
     errLowest = evaluateModel(modelBest, testData)
-    for i in (0,k):
 
+    for i in range(3):
         (trainData, validationData, testData) = prepare_data(sc)
         trainData.persist()
         validationData.persist()
         testData.persist()
         model = trainEvaluateModel(trainData)
         err = evaluateModel(model, testData)
-	if err <= errLowest:
-	    errLowest = err
-	    modelBest = model
-    	rocCurve(modelBest, validationData)
+        if err <= errLowest:
+            errLowest = err
+            modelBest = model
+            rocCurve(modelBest, validationData)
     predictData(sc, modelBest)
